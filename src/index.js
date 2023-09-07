89148997d59113605ae259719f58955b766bb6fe
Leandro Echegorria <leandro.echegorria@gmail.com>
2023-09-07 00:48:10 -0300

    chat

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import { __dirname } from './path.js'
 import path from 'path';
 import { engine } from 'express-handlebars';
 import { Server } from 'socket.io'
-//import { ProductManager } from './controllers/productManager.js';
+import { ProductManager } from './controllers/productManager.js';
 import mongoose from 'mongoose';
+
 import productRouter from './routes/products.routes.js';
 import cartRouter from './routes/carts.routes.js';
+import messageRouter from './routes/messages.routes.js';
+
+import messageModel from './models/messages.models.js';
+import productModel from './models/products.models.js';
 
 const app = express()
 const PORT = 8080
@@ -16,17 +21,21 @@ mongoose.connect(mongoURL)
   .then(() => console.log("DB conectada"))
   .catch((error) => console.log("Error en conexion a MongoDB Atlas: ", error))
 
+
+
 //Server
 const server = app.listen(PORT, ()=> {
   console.log(`Server on port ${PORT}, http://localhost:${PORT}/`)
 })
+
 const io = new Server(server)
 
+//Product Manager
+const ProductManagerSocket = new ProductManager('./src/models/product.json')
 
 //middlewares
 app.use(express.json())
 app.use(express.urlencoded ({extended: true}))
-
 app.engine('handlebars', engine()) //def trabajo con hbs
 app.set('view engine', 'handlebars') //no hace falta explicitar el .handlebars en los archivos
 app.set('views', path.resolve(__dirname, './views'))
@@ -35,7 +44,7 @@ app.set('views', path.resolve(__dirname, './views'))
 app.use('/static', express.static(path.join(__dirname, '/public')))
 app.use('/api/product', productRouter)
 app.use('/api/carts', cartRouter)   
-//app.use('/api/carts', cartRouter)   
+app.use('/api/messages', messageRouter)   
 
 app.get('/', (req,res)=> {
   res.send("Bienvenido")
@@ -48,7 +57,7 @@ app.get('/static', async (req,res) => {
   res.render('home', { 
     //se mostraran los productos utilizando HBS
     titulo: "Products",
-    rutaCSS : "home",
+    rutaCSS: "home",
     rutaJS: "home",
     productos: prods 
 
@@ -58,11 +67,18 @@ app.get('/static', async (req,res) => {
 app.get('/static/realTimeProducts', (req,res) => {
   res.render('realTimeProducts', {
     titulo: "Real Time Products",
-    rutaCSS : "realTimeProducts",
+    rutaCSS: "realTimeProducts",
     rutaJS: "realTimeProducts",
 
   })
 })
+
+app.get('/static/chat', (req, res) => {
+	res.render('chat', {
+		rutaCSS: 'chat',
+		rutaJS: 'chat',
+	});
+});
 
 
 
@@ -70,15 +86,26 @@ app.get('/static/realTimeProducts', (req,res) => {
 io.on("connection", (socket) => {
   console.log("Conexion con Socket.io")
 
-  socket.on('nuevoProducto', (prod) => {
-      console.log(prod)
-      ProductManagerSocket.addProduct(prod)
-
-      socket.emit("mensajeProductoCreado", "El producto se creo correctamente")
+  socket.on('nuevoProducto', async(prod) => {
+    await productModel.create(prod)
+    
+    socket.emit("mensajeProductoCreado", "El producto se creo correctamente")
   })
   socket.on('productos', async() => {
-    const prods = await ProductManagerSocket.getProducts()
+    const prods = await productModel.find()
     socket.emit('getProducts', prods)
+  })
+
+
+  socket.on('mensaje', async info => {
+    const {email, message} = info
+    await messageModel.create({
+      email,
+      message
+    })
+    const messages = await messageModel.find()
+
+    io.emit('mensajes', messages)
   })
 
 })
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,8 +1,9 @@
-import { Router } from 'express';
-//import { CartManager } from '../controllers/cartManager.js';
+import { Router } from 'express'
+//import { CartManager } from '../controllers/cartManager.js'
 import cartModel from "../models/carts.models.js"
+import productModel from '../models/products.models.js'
 
-//const cartManager = new CartManager('./src/models/cart.json', './src/models/productos.json');
+//const cartManager = new CartManager('./src/models/cart.json', './src/models/productos.json')
 
 const cartRouter = Router();
 
@@ -39,7 +40,7 @@ cartRouter.get('/:cid', async (req, res) => {
             : res.status(404).send({resultado:'Not Found', message: cart})
 
     }catch (error){
-        res.status(400).send ({ error:`Error al consultar carrito: ${error}`});
+        res.status(400).send ({ error:`Error al consultar carrito: ${error}`})
     }
 });
 
@@ -55,7 +56,7 @@ cartRouter.put('/:cid', async (req, res) => {
         cart ? res.status(200).send({resultado: 'OK', message: cart})
             : res.status(404).send({resultado:'Not Found', message: cart})
     } catch(error){
-        res.status(400).send ({ error:`Error al actualizar carritos: ${error}`});
+        res.status(400).send ({ error:`Error al actualizar carritos: ${error}`})
     };
 });
 
@@ -66,19 +67,35 @@ cartRouter.delete('/:cid', async (req, res) => {
         cart ? res.status(200).send({resultado: 'OK', message: cart})
         : res.status(404).send({resultado:'Not Found', message: cart})
     } catch (error){
-        res.status(400).send ({ error:`Error al eliminar carritos: ${error}`});
+        res.status(400).send ({ error:`Error al eliminar carritos: ${error}`})
 
     }
 })
 
 cartRouter.post('/:cid/product/:pid', async (req, res) => {
     const {cid, pid} = req.params;
-    const {quantity} = req.body;
-    const success = await cartModel.addProductToCart(cid, pid, quantity);
-    
-    (success) ? res.status(200).send("Product added to cart") : res.status(404).send("Cart not found");
-    
-});
+
+    try{
+        const cart = await cartModel.findById(cid)
+        const product = await productModel.findById(pid)
+
+        if (!product) {
+            res.status(404).send({resultado: "Product not found", message: product})
+            return false
+        }
+        
+        if (cart) {
+            const productExists = cart.product.find(prod => prod.id_prod == pid)
+            productExists ? productExists.quantity++ : cart.products.push({ id_prod: product._id, quantity: 1 })
+			await cart.save();
+			res.status(200).send({ resultado: 'OK', message: cart })
+		} else {
+			res.status(404).send({ resultado: 'Cart Not Found', message: cart })
+		}
+	} catch (error) {
+		res.status(400).send({ error: `Error al crear producto: ${error}` })
+	}
+}) 
 
 
 export default cartRouter;
